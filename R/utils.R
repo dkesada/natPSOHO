dc4e8257f2dbbe6ba21d4b40604e8dc05d37917b
dkesada <dkesada@gmail.com>
2020-12-16 13:27:40 +0100

    Utils functions

---
+++ R/utils.R
@@ -0,0 +1,23 @@
+#' One hot encoder for natural numbers without the 0.
+#' 
+#' Given a natural number, return the natural number equivalent to its
+#' one-hot encoding. Examples: 3 -> 100 -> 4, 5 -> 10000 -> 16
+#' 
+#' @param nat the natural number to convert
+#' @return the converted number
+one_hot <- function(nat){
+  return(2^(nat-1))
+}
+
+#' Geometric distribution sampler truncated to a maximum
+#' 
+#' A geometric distribution sampler with probability 'p' restricted to values
+#' inside [1, max]. Because of this restriction, very low values of 'p' return
+#' increasingly uniform populations in the interval [1, max].
+#' 
+#' @param p the parameter of the geometric distribution
+#' @param max the maximum value allowed to be sampled
+#' @return the sampled value
+trunc_geom <- function(p, max){
+  return(floor(log(1 - runif(1)*(1 - (1 - p)^max)) / log(1 - p)) + 1)
+}
